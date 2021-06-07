bf3cb83290dc8bdfa69178cb360f1246eb1f45c5
ayusmanadhikari <unixparadoxfb@gmail.com>
2021-06-07 17:17:05 +0545

    emailbackend

--- website/users/customauth.py
+++ website/users/customauth.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
@@ -5,8 +6,9 @@ from django.core.exceptions import ObjectDoesNotExist
 
 class EmailBackEnd(ModelBackend):
     def authenticate(self, request, username=None, password=None):
+        UserModel = get_user_model()
         try:
-            user = User.objects.get(email=username)
+            user = UserModel.objects.get(email=username)
         except ObjectDoesNotExist:
             return None
         else:
