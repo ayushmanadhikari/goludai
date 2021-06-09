49f91ad3cb0d4ecb7eb98785fb60c4d483a8418f
ayushmanadhikari <unixparadoxfb@gmail.com>
2021-06-09 09:33:30 +0545

    pagination and userpostlistview

--- website/blog/views.py
+++ website/blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth import get_user_model
 
 def home(request):
     return render(request, 'blog/home.html')
@@ -12,6 +13,20 @@ class HomeListView(ListView):
     model = Post                        #post_list.html         --default
     template_name = 'blog/home.html'
     context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post                        #post_list.html         --default
+    template_name = 'blog/user_post.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        UserModel = get_user_model()
+        user = get_object_or_404(UserModel, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
