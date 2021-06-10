52ccd5ea7828eae7dc405968ee1bad58112323fc
ayushmanadhikari <unixparadoxfb@gmail.com>
2021-06-10 09:17:27 +0545

    password reset added

--- website/website/settings.py
+++ website/website/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
-    'users',
+    'users.apps.UsersConfig',
     'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -146,3 +146,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('GMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('PASSWORD')
+
+
